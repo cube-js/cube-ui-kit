2bb0723092fcfd3c71198f5bac63bfd88983fbb9
Andrey Yamanov <tenphi@gmail.com>
2020-12-15 10:45:30 +0300

    fix: clean up css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GlobalStyles from '../src/components/CSSCustomProperties';
+import GlobalStyles from '../src/components/GlobalStyles';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
