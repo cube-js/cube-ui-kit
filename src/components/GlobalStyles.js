2d8b0edf1a3dcddb8f204ff6c50c2fb3dabc6ab8
Andrey Yamanov <tenphi@gmail.com>
2020-12-15 18:20:26 +0300

    fix: missing fonts issue

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -18,7 +18,7 @@ const GlobalStyles = createGlobalStyle`
   
   body {
     background-color: white !important;
-    font-family: 'Inter', -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     margin: 0;
@@ -28,38 +28,37 @@ const GlobalStyles = createGlobalStyle`
   code {
     font-family: 'Source Code Pro', Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
-
-
+  
   @font-face {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 400;
-    font-display: swap;
-    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Regular.woff2);
+    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Regular.woff2) format('woff2'),
+      url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Regular.woff) format('woff');
   }
   @font-face {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 500;
-    font-display: swap;
-    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Medium.woff2);
+    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Medium.woff2) format('woff2'),
+      url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Medium.woff) format('woff');
   }
   @font-face {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 600;
-    font-display: swap;
-    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-SemiBold.woff2);
+    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-SemiBold.woff2) format('woff2'),
+      url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-SemiBold.woff) format('woff');
   }
   @font-face {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
-    font-display: swap;
-    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Bold.woff2);
+    src: url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Bold.woff2) format('woff2'),
+      url(${({ publicUrl }) => publicUrl || ''}/fonts/Inter-Bold.woff) format('woff');
   }
   @font-face {
-    font-family: "Source Code Pro";
+    font-family: 'Source Code Pro';
     src: url('${({ publicUrl }) =>
       publicUrl || ''}/fonts/SourceCodePro-Regular.woff2') format('woff2'),
       url('${({ publicUrl }) =>
