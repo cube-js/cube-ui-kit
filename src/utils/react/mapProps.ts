0505590ec7a6160953a91c1558aef4fa20cef042
Andrey Yamanov <tenphi@gmail.com>
2021-08-05 19:49:47 +0300

    refactor(@cubejs-enterprise/uikit): initial typescript support * 6

---
+++ src/utils/react/mapProps.ts
@@ -0,0 +1,26 @@
+import { CubeButtonProps } from '../../atoms/Button/Button';
+import { AriaButtonProps } from '@react-types/button';
+
+/** Converts AriaButtonProps to CubeButtonProps */
+export function ariaToCubeButtonProps(
+  props: AriaButtonProps<'button'>,
+): CubeButtonProps {
+  const { type, ...filteredProps } = props;
+
+  return {
+    ...filteredProps,
+    htmlType: type,
+  };
+}
+
+/** Converts CubeButtonProps to AriaButtonProps */
+export function cubeToAriaButtonProps(
+  props: CubeButtonProps,
+): AriaButtonProps<'button'> {
+  const { htmlType, ...filteredProps } = props;
+
+  return {
+    ...filteredProps,
+    type: htmlType,
+  };
+}
