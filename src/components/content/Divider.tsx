909688ab2db1544dc6282d71d6884e72c966ae9b
Andrey Yamanov <tenphi@gmail.com>
2021-11-05 14:03:16 +0300

    fix(Divider): fix style props

--- src/components/content/Divider.tsx
+++ src/components/content/Divider.tsx
@@ -1,10 +1,12 @@
 import { forwardRef } from 'react';
 import { Base } from '../Base';
-import { OUTER_STYLES } from '../../styles/list';
+import { OUTER_STYLES, BASE_STYLES } from '../../styles/list';
 import { extractStyles } from '../../utils/styles';
 import { filterBaseProps } from '../../utils/filterBaseProps';
 import { useSlotProps } from '../../utils/react';
 import { BaseProps, OuterStyleProps } from '../types';
+
+const STYLE_LIST = [...OUTER_STYLES, ...BASE_STYLES];
 
 const DEFAULT_STYLES = {
   gridArea: 'divider',
@@ -20,7 +22,7 @@ export interface CubeDividerProps extends BaseProps, OuterStyleProps {}
 export const Divider = forwardRef((props: CubeDividerProps, ref) => {
   props = useSlotProps(props, 'divider');
 
-  const styles = extractStyles(props, OUTER_STYLES, DEFAULT_STYLES);
+  const styles = extractStyles(props, STYLE_LIST, DEFAULT_STYLES);
 
   return (
     <Base
