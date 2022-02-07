82731be231f9ed8c721a496359101957fd3edd7f
Ruslan Martynov <mkheroes@ya.ru>
2022-02-07 20:12:48 +1000

    feat: added component Result

--- src/utils/react/wrapNodeIfPlain.ts
+++ src/utils/react/wrapNodeIfPlain.ts
@@ -4,7 +4,7 @@ import { isFragment } from 'react-is';
 export function wrapNodeIfPlain(children: ReactNode, render: () => ReactNode) {
   const childrenIsFragment = Array.isArray(children) || isFragment(children);
 
-  if (!!childrenIsFragment || isValidElement(children)) {
+  if (!childrenIsFragment && isValidElement(children)) {
     return children;
   }
 
