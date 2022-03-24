00da7343e5f9352f501bfaaef9d40902e6c67d28
Sergey Garin <sergey.garin@cube.dev>
2022-03-24 18:27:11 +0400

    feat: deprecate props field in styled()

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-// const fs = require('fs')
-
-// const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'))
-
 // @TODO: move to .swcrc when main build moved to swc
 const config = {
   sourceMaps: true,
@@ -11,6 +7,8 @@ const config = {
       syntax: 'typescript',
       tsx: true,
     },
+
+    target: 'es2021',
 
     transform: {
       react: {
@@ -23,10 +21,7 @@ const config = {
 module.exports = {
   verbose: true,
   coverageDirectory: './coverage/',
-  transform: {
-    '^.+\\.(t|j)sx?$': [
-      '@swc/jest',
-      { ...config /* custom configuration in jest */ },
-    ],
-  },
+  testEnvironment: '@happy-dom/jest-environment',
+  transform: { '^.+\\.(t|j)sx?$': ['@swc/jest', config] },
+  setupFilesAfterEnv: ['./src/test/setup.ts'],
 };
