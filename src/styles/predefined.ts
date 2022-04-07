885f555f07a5fcbab6e8f0b3a0825a9ad39d8ad3
Sergey Garin <sergikgarin@gmail.com>
2022-04-07 17:27:32 +0400

    fix: get rid of side effects in the package (#69)
    * fix: get rid of side effects in the package
    
    * feat: add canary deployment

--- src/styles/predefined.ts
+++ src/styles/predefined.ts
@@ -1,8 +1,13 @@
+import {
+  RawStyleHandler,
+  StyleHandler,
+  styleHandlerCacheWrapper,
+} from '../utils/styles';
 import { gapStyle } from './gap';
+import { fillStyle } from './fill';
 import { flowStyle } from './flow';
 import { resetStyle } from './reset';
 import { colorStyle } from './color';
-import { fillStyle } from './fill';
 import { widthStyle } from './width';
 import { heightStyle } from './height';
 import { radiusStyle } from './radius';
@@ -25,7 +30,7 @@ import { styledScrollbarStyle } from './styledScrollbar';
 import { displayStyle } from './display';
 import { alignStyle } from './align';
 import { justifyStyle } from './justify';
-import { defineCustomStyle, defineStyleAlias } from './index';
+import { createStyle } from './createStyle';
 
 const numberConverter = (val) => {
   if (typeof val === 'number') {
@@ -49,69 +54,125 @@ const rowsConverter = (val) => {
   return;
 };
 
-// Manually define styles that are used in other custom styles.
-// Otherwise, they won't be handled as expected.
-defineStyleAlias('fontSize');
-defineStyleAlias('lineHeight');
-defineStyleAlias('fontWeight');
-defineStyleAlias('fontStyle');
-defineStyleAlias('letterSpacing');
-defineStyleAlias('textTransform');
-defineStyleAlias('fontFamily');
-defineStyleAlias('paddingTop');
-defineStyleAlias('paddingRight');
-defineStyleAlias('paddingBottom');
-defineStyleAlias('paddingLeft');
-defineStyleAlias('marginTop');
-defineStyleAlias('marginRight');
-defineStyleAlias('marginBottom');
-defineStyleAlias('marginLeft');
-// Coordinates
-defineStyleAlias('top', 'top', numberConverter);
-defineStyleAlias('right', 'right', numberConverter);
-defineStyleAlias('bottom', 'bottom', numberConverter);
-defineStyleAlias('left', 'left', numberConverter);
-// Style aliases
-defineStyleAlias('gridAreas', 'grid-template-areas');
-defineStyleAlias('gridColumns', 'grid-template-columns', columnsConverter);
-defineStyleAlias('gridRows', 'grid-template-rows', rowsConverter);
-defineStyleAlias('gridTemplate', 'grid-template', (val) => {
-  if (typeof val !== 'string') return;
+type StyleHandlerMap = Record<string, StyleHandler[]>;
 
-  return val
-    .split('/')
-    .map((s, i) => (i ? columnsConverter : rowsConverter)(s))
-    .join('/');
-});
+export const STYLE_HANDLER_MAP: StyleHandlerMap = {};
 
-[
-  displayStyle,
-  transitionStyle,
-  resetStyle,
-  fillStyle,
-  widthStyle,
-  marginStyle,
-  marginBlockStyle,
-  marginInlineStyle,
-  gapStyle,
-  flowStyle,
-  colorStyle,
-  heightStyle,
-  radiusStyle,
-  borderStyle,
-  shadowStyle,
-  paddingStyle,
-  paddingBlockStyle,
-  paddingInlineStyle,
-  alignStyle,
-  justifyStyle,
-  presetStyle,
-  boxShadowCombinator,
-  outlineStyle,
-  fontStyle,
-  fontStyleStyle,
-  groupRadiusAttr,
-  styledScrollbarStyle,
-]
-  // @ts-ignore
-  .forEach((handler) => defineCustomStyle(handler));
+export function defineCustomStyle(
+  names: string[] | StyleHandler,
+  handler?: RawStyleHandler,
+) {
+  let handlerWithLookup: StyleHandler;
+
+  if (typeof names === 'function') {
+    handlerWithLookup = names;
+    names = handlerWithLookup.__lookupStyles;
+  } else if (handler) {
+    handlerWithLookup = Object.assign(handler, { __lookupStyles: names });
+  } else {
+    console.warn('CubeUIKit: incorrect custom style definition: ', names);
+    return;
+  }
+
+  handlerWithLookup = styleHandlerCacheWrapper(handlerWithLookup);
+
+  if (Array.isArray(names)) {
+    // just to pass type checking
+    names.forEach((name) => {
+      if (!STYLE_HANDLER_MAP[name]) {
+        STYLE_HANDLER_MAP[name] = [];
+      }
+
+      STYLE_HANDLER_MAP[name].push(handlerWithLookup);
+    });
+  }
+}
+
+type ConverterHandler = (
+  s: string | boolean | number | undefined,
+) => string | undefined;
+
+export function defineStyleAlias(
+  styleName: string,
+  cssStyleName?: string,
+  converter?: ConverterHandler,
+) {
+  const styleHandler = createStyle(styleName, cssStyleName, converter);
+
+  if (!STYLE_HANDLER_MAP[styleName]) {
+    STYLE_HANDLER_MAP[styleName] = [];
+  }
+
+  STYLE_HANDLER_MAP[styleName].push(styleHandler);
+}
+
+export function predefine() {
+  // Manually define styles that are used in other custom styles.
+  // Otherwise, they won't be handled as expected.
+  defineStyleAlias('fontSize');
+  defineStyleAlias('lineHeight');
+  defineStyleAlias('fontWeight');
+  defineStyleAlias('fontStyle');
+  defineStyleAlias('letterSpacing');
+  defineStyleAlias('textTransform');
+  defineStyleAlias('fontFamily');
+  defineStyleAlias('paddingTop');
+  defineStyleAlias('paddingRight');
+  defineStyleAlias('paddingBottom');
+  defineStyleAlias('paddingLeft');
+  defineStyleAlias('marginTop');
+  defineStyleAlias('marginRight');
+  defineStyleAlias('marginBottom');
+  defineStyleAlias('marginLeft');
+  // Coordinates
+  defineStyleAlias('top', 'top', numberConverter);
+  defineStyleAlias('right', 'right', numberConverter);
+  defineStyleAlias('bottom', 'bottom', numberConverter);
+  defineStyleAlias('left', 'left', numberConverter);
+  // Style aliases
+  defineStyleAlias('gridAreas', 'grid-template-areas');
+  defineStyleAlias('gridColumns', 'grid-template-columns', columnsConverter);
+  defineStyleAlias('gridRows', 'grid-template-rows', rowsConverter);
+  defineStyleAlias('gridTemplate', 'grid-template', (val) => {
+    if (typeof val !== 'string') return;
+
+    return val
+      .split('/')
+      .map((s, i) => (i ? columnsConverter : rowsConverter)(s))
+      .join('/');
+  });
+
+  [
+    displayStyle,
+    transitionStyle,
+    resetStyle,
+    fillStyle,
+    widthStyle,
+    marginStyle,
+    marginBlockStyle,
+    marginInlineStyle,
+    gapStyle,
+    flowStyle,
+    colorStyle,
+    heightStyle,
+    radiusStyle,
+    borderStyle,
+    shadowStyle,
+    paddingStyle,
+    paddingBlockStyle,
+    paddingInlineStyle,
+    alignStyle,
+    justifyStyle,
+    presetStyle,
+    boxShadowCombinator,
+    outlineStyle,
+    fontStyle,
+    fontStyleStyle,
+    groupRadiusAttr,
+    styledScrollbarStyle,
+  ]
+    // @ts-ignore
+    .forEach((handler) => defineCustomStyle(handler));
+
+  return { STYLE_HANDLER_MAP, defineCustomStyle, defineStyleAlias };
+}
