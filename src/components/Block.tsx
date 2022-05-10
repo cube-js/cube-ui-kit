67a89126b49acf1a377b9efa2330099a453528ca
Andrey Yamanov <tenphi@gmail.com>
2022-05-10 12:01:03 +0400

    chore: migrate to tasty()

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -3,21 +3,23 @@ import { CONTAINER_STYLES } from '../styles/list';
 import { extractStyles } from '../utils/styles';
 import { filterBaseProps } from '../utils/filterBaseProps';
 import { AllBaseProps, ContainerStyleProps } from './types';
-import { Base } from './Base';
+import { tasty } from '../tasty';
 
-export const CUBE_BLOCK_STYLES = {
-  display: 'block',
-};
+const RawBlock = tasty({
+  styled: {
+    display: 'block',
+  },
+});
 
 export interface CubeBlockProps
   extends Omit<AllBaseProps, keyof ContainerStyleProps | 'as'>,
     ContainerStyleProps {}
 
 export const Block = forwardRef((props: CubeBlockProps, ref) => {
-  const styles = extractStyles(props, CONTAINER_STYLES, CUBE_BLOCK_STYLES);
+  const styles = extractStyles(props, CONTAINER_STYLES);
 
   return (
-    <Base
+    <RawBlock
       {...filterBaseProps(props, { eventProps: true })}
       styles={styles}
       ref={ref}
