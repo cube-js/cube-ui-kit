1bca3775cd28c894e93fc510ea700481331f3857
Andrey Yamanov <tenphi@gmail.com>
2022-06-03 18:43:31 +0400

    fix(Button): update stories and test

--- src/components/actions/Button/Button.test.tsx
+++ src/components/actions/Button/Button.test.tsx
@@ -4,11 +4,11 @@ import '@testing-library/jest-dom/extend-expect';
 // import { expect } from '@open-wc/testing';
 
 //👇 Imports a specific story for the test
-import { Primary } from './Button.stories';
+import { Default } from './Button.stories';
 
 it('renders the button in the primary state', () => {
   // render(ReactDOM.render(createElement(Primary, Primary.args), document.body));
   // @ts-ignore
-  render(<Primary {...Primary.args} />);
+  render(<Default {...Default.args} />);
   expect(screen.getByRole('button')).toHaveTextContent('Button');
 });
