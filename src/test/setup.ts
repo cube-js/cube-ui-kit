e1635fd28bb1b8d0fb5badb33235434ef51f6bdc
Andrey Yamanov <tenphi@gmail.com>
2022-06-13 14:28:44 +0400

    chore: update deps

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,6 +5,7 @@ import { configure } from '@testing-library/react';
 import { config } from 'react-transition-group';
 import { AbortController } from 'node-abort-controller';
 
+// @ts-ignore
 global.AbortController = AbortController;
 config.disabled = true;
 
