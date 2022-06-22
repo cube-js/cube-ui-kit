d53ef4285814e71c4ae747de41f16d7e07fe179a
Sergey Garin <sergey.garin@cube.dev>
2022-06-22 18:34:12 +0400

    fix: spin animations

--- src/components/status/Spin/SpinCube.tsx
+++ src/components/status/Spin/SpinCube.tsx
@@ -19,7 +19,7 @@ export const SpinCube = memo(styled.div.attrs<SpinCubeProps>(
 )`
   --cube-spin-cube-size: calc(var(--cube-spin-size) / 3);
   --cube-spin-cube-border-width: calc(4 / 100 * var(--cube-spin-size));
-  --cube-spin-cube-position: calc((10 / 100) * var(--cube-spin-size));
+  --cube-spin-cube-position: calc(10 / 100 * var(--cube-spin-size));
 
   box-sizing: content-box;
   position: absolute;
@@ -31,10 +31,10 @@ export const SpinCube = memo(styled.div.attrs<SpinCubeProps>(
   overflow: hidden;
   contain: size layout style paint;
 
+  animation-name: var(--cube-spin-animation-name);
   animation-duration: 2s;
   animation-iteration-count: infinite;
   animation-timing-function: cubic-bezier(0.53, 0.11, 0.32, 0.84);
-  animation-name: var(--cube-spin-animation-name);
 
   @media (prefers-reduced-motion) {
     animation-play-state: paused;
@@ -56,62 +56,38 @@ export const SpinCube = memo(styled.div.attrs<SpinCubeProps>(
     0% {
       transform: translate(0%, 0%);
     }
-    9.09% {
+    9% {
       transform: translate(100%, 0%);
     }
-    27.27% {
+    18% {
       transform: translate(100%, 0%);
     }
-    36.36% {
+    27% {
+      transform: translate(100%, 0%);
+    }
+    36% {
       transform: translate(100%, 100%);
     }
-    54.54% {
+    45% {
       transform: translate(100%, 100%);
     }
-    63.63% {
+    54% {
+      transform: translate(100%, 100%);
+    }
+    63% {
       transform: translate(0%, 100%);
     }
-    81.81% {
+    72% {
       transform: translate(0%, 100%);
     }
-    90.9% {
+    81% {
+      transform: translate(0%, 100%);
+    }
+    90% {
       transform: translate(0%, 0%);
     }
     100% {
       transform: translate(0%, 0%);
-    }
-  }
-
-  @keyframes cube-spin-bottom {
-    0% {
-      transform: translate(0%, 100%);
-    }
-    8.3% {
-      transform: translate(0%, 100%);
-    }
-    16.6% {
-      transform: translate(0%, 0%);
-    }
-    33.3% {
-      transform: translate(0%, 0%);
-    }
-    41.6% {
-      transform: translate(100%, 0%);
-    }
-    58.3% {
-      transform: translate(100%, 0%);
-    }
-    66.6% {
-      transform: translate(100%, 100%);
-    }
-    83.3% {
-      transform: translate(100%, 100%);
-    }
-    91.6% {
-      transform: translate(0%, 100%);
-    }
-    100% {
-      transform: translate(0%, 100%);
     }
   }
 
@@ -119,29 +95,77 @@ export const SpinCube = memo(styled.div.attrs<SpinCubeProps>(
     0% {
       transform: translate(100%, 100%);
     }
-    16.6% {
+    9% {
       transform: translate(100%, 100%);
     }
-    25% {
+    18% {
+      transform: translate(100%, 100%);
+    }
+    27% {
       transform: translate(0%, 100%);
     }
-    41.6% {
+    36% {
       transform: translate(0%, 100%);
     }
-    50% {
+    45% {
+      transform: translate(0%, 100%);
+    }
+    54% {
       transform: translate(0%, 0%);
     }
-    66.6% {
+    63% {
       transform: translate(0%, 0%);
     }
-    75% {
+    72% {
+      transform: translate(0%, 0%);
+    }
+    81% {
       transform: translate(100%, 0%);
     }
-    91.6% {
+    90% {
       transform: translate(100%, 0%);
     }
     100% {
       transform: translate(100%, 100%);
+    }
+  }
+
+  @keyframes cube-spin-bottom {
+    0% {
+      transform: translate(0%, 100%);
+    }
+    9% {
+      transform: translate(0%, 100%);
+    }
+    18% {
+      transform: translate(0%, 0%);
+    }
+    27% {
+      transform: translate(0%, 0%);
+    }
+    36% {
+      transform: translate(0%, 0%);
+    }
+    45% {
+      transform: translate(100%, 0%);
+    }
+    54% {
+      transform: translate(100%, 0%);
+    }
+    63% {
+      transform: translate(100%, 0%);
+    }
+    72% {
+      transform: translate(100%, 100%);
+    }
+    81% {
+      transform: translate(100%, 100%);
+    }
+    90% {
+      transform: translate(100%, 100%);
+    }
+    100% {
+      transform: translate(0%, 100%);
     }
   }
 `);
