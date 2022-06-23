a4fe63aeebbca0c8795d6a0d6f42d1ed23a49972
Andrey Yamanov <tenphi@gmail.com>
2022-06-23 15:46:49 +0400

    docs(Field): add stories * 2

--- src/components/forms/Form/Field.stories.tsx
+++ src/components/forms/Form/Field.stories.tsx
@@ -16,26 +16,26 @@ const Template: StoryFn<typeof Field> = (args) => {
   );
 };
 
-export const Default: StoryFn<typeof Field> = Template.bind({});
+export const Default = Template.bind({});
 Default.args = {};
 
-export const WithTooltip: StoryFn<typeof Field> = Template.bind({});
+export const WithTooltip = Template.bind({});
 WithTooltip.args = {
   tooltip: 'Long description',
 };
 
-export const WithMessage: StoryFn<typeof Field> = Template.bind({});
+export const WithMessage = Template.bind({});
 WithMessage.args = {
   message: 'Inline description of the field',
 };
 
-export const WithErrorMessage: StoryFn<typeof Field> = Template.bind({});
+export const WithErrorMessage = Template.bind({});
 WithErrorMessage.args = {
   message: 'This field is required',
   validationState: 'error',
 };
 
-export const Styled: StoryFn<typeof Field> = Template.bind({});
+export const Styled = Template.bind({});
 Styled.args = {
   styles: {
     width: '30x',
