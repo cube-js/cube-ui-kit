15ecfbe69fb1f821aa5c21842d975dac835fce38
Andrey Yamanov <tenphi@gmail.com>
2022-09-15 18:07:02 +0200

    fix: srr * 2

---
+++ src/utils/react/useViewportSize.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+interface ViewportSize {
+  width: number;
+  height: number;
+}
+
+// @ts-ignore
+let visualViewport = typeof window !== 'undefined' && window.visualViewport;
+
+export function useViewportSize(): ViewportSize {
+  let [size, setSize] = useState(() => getViewportSize());
+
+  useEffect(() => {
+    // Use visualViewport api to track available height even on iOS virtual keyboard opening
+    let onResize = () => {
+      setSize((size) => {
+        let newSize = getViewportSize();
+        if (newSize.width === size.width && newSize.height === size.height) {
+          return size;
+        }
+        return newSize;
+      });
+    };
+
+    if (!visualViewport) {
+      window.addEventListener('resize', onResize);
+    } else {
+      visualViewport.addEventListener('resize', onResize);
+    }
+
+    return () => {
+      if (!visualViewport) {
+        window.removeEventListener('resize', onResize);
+      } else {
+        visualViewport.removeEventListener('resize', onResize);
+      }
+    };
+  }, []);
+
+  return size;
+}
+
+function getViewportSize(): ViewportSize {
+  return {
+    width:
+      (visualViewport && visualViewport.width) ||
+      (typeof window !== 'undefined' ? window.innerWidth : 0),
+    height:
+      (visualViewport && visualViewport.height) ||
+      (typeof window !== 'undefined' ? window.innerHeight : 0),
+  };
+}
