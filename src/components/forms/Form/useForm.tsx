ee7a4a9e535abc0dd6d0e4cae918bc8eb8780f33
Andrey Yamanov <tenphi@gmail.com>
2022-09-16 13:05:23 +0200

    fix(Form): type into ComboBox to the initial value * 2

--- src/components/forms/Form/useForm.tsx
+++ src/components/forms/Form/useForm.tsx
@@ -149,7 +149,7 @@ export class CubeFormInstance<
   ) {
     const field = this.fields[name];
 
-    if (!field || isEqual(value, field.inputValue)) {
+    if (!field || isEqual(value, inputOnly ? field.inputValue : field.value)) {
       return;
     }
 
