27b8198db2c9a549b537a857964dca9fd590e82a
Sergey Garin <sergey.garin@cube.dev>
2022-11-16 10:34:57 +0400

    [CC-1616] Recalculate position of a drodown on filter change (#243)

--- src/components/pickers/ComboBox/ComboBox.stories.tsx
+++ src/components/pickers/ComboBox/ComboBox.stories.tsx
@@ -1,5 +1,6 @@
 import { DollarCircleOutlined } from '@ant-design/icons';
 import { Meta, Story } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 
 import { SELECTED_KEY_ARG } from '../../../stories/FormFieldArgs';
 import { baseProps } from '../../../stories/lists/baseProps';
@@ -10,7 +11,7 @@ export default {
   title: 'Pickers/ComboBox',
   component: ComboBox,
   subcomponents: { Item: ComboBox.Item },
-  args: { id: 'name', width: '200px' },
+  args: { id: 'name', width: '200px', label: 'Choose your favourite color' },
   parameters: { controls: { exclude: baseProps } },
   argTypes: { ...SELECTED_KEY_ARG },
 } as Meta<CubeComboBoxProps<any>>;
@@ -62,3 +63,35 @@ export const Wide: Story<CubeComboBoxProps<any>> = (args) => (
 );
 
 Wide.args = { width: '600px', defaultSelectedKey: 'red' };
+
+export const With1LongOption: Story<CubeComboBoxProps<any>> = (args) => (
+  <ComboBox {...args}>
+    <ComboBox.Item key="red">Red</ComboBox.Item>
+    <ComboBox.Item key="orange">Orange</ComboBox.Item>
+    <ComboBox.Item key="yellow">Yellow</ComboBox.Item>
+    <ComboBox.Item key="green">
+      green lorem ipsum dolor sit amet, consectetur adipiscing elit
+    </ComboBox.Item>
+    <ComboBox.Item key="blue">Blue</ComboBox.Item>
+    <ComboBox.Item key="purple">Purple</ComboBox.Item>
+    <ComboBox.Item key="violet">Violet</ComboBox.Item>
+  </ComboBox>
+);
+With1LongOption.parameters = { layout: 'centered' };
+With1LongOption.play = async ({ canvasElement }) => {
+  const { getByRole } = within(canvasElement);
+
+  const openButton = getByRole('button');
+
+  await userEvent.click(openButton);
+};
+
+export const With1LongOptionFiltered = With1LongOption.bind({});
+With1LongOptionFiltered.parameters = { layout: 'centered' };
+With1LongOptionFiltered.play = async ({ canvasElement }) => {
+  const { getByRole } = within(canvasElement);
+
+  const combobox = getByRole('combobox');
+
+  await userEvent.type(combobox, 'Red');
+};
