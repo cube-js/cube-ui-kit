03f6c6b523e73e8307f655a0b305ba950590f732
Andrey Yamanov <tenphi@gmail.com>
2023-01-16 18:33:50 +0100

    fix(Button): special neutral styles * 2

--- src/components/actions/Button/Button.tsx
+++ src/components/actions/Button/Button.tsx
@@ -266,8 +266,6 @@ const SpecialButtonElement = tasty({
       // default
       '': '#white',
 
-      '[data-type="neutral"]': '#white',
-      '[data-type="neutral"] & hovered': '#white.75',
       '[data-type="clear"]': '#purple',
 
       // other
