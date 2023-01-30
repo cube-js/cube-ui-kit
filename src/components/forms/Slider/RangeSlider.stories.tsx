a5de8a396078793454ce76fdc8b665bb4be510bf
Andrey Yamanov <tenphi@gmail.com>
2023-01-30 16:06:45 +0100

    fix(Slider): add more stories

--- src/components/forms/Slider/RangeSlider.stories.tsx
+++ src/components/forms/Slider/RangeSlider.stories.tsx
@@ -7,7 +7,14 @@ import { RangeSlider, CubeRangeSliderProps } from './RangeSlider';
 export default {
   title: 'Forms/RangeSlider',
   component: RangeSlider,
-  args: { id: 'name', width: '100%' },
+  args: {
+    id: 'name',
+    width: '100%',
+    defaultValue: { start: 4, end: 12 },
+    minValue: 0,
+    maxValue: 20,
+    step: 2,
+  },
   parameters: { controls: { exclude: baseProps } },
 } as Meta<CubeRangeSliderProps>;
 
@@ -16,12 +23,16 @@ const Template: Story<CubeRangeSliderProps> = (args) => (
 );
 
 export const Single = Template.bind({});
-
 Single.args = {
-  defaultValue: { start: 4, end: 12 },
-  minValue: 0,
-  maxValue: 20,
-  step: 2,
   label: 'Slider',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  isDisabled: true,
+};
+
+export const WithGradation = Template.bind({});
+WithGradation.args = {
   gradation: ['0', '50', '100'],
 };
--- src/components/forms/Slider/Slider.stories.tsx
+++ src/components/forms/Slider/Slider.stories.tsx
@@ -7,29 +7,33 @@ import { Slider, CubeSliderProps } from './Slider';
 export default {
   title: 'Forms/Slider',
   component: Slider,
-  args: { id: 'name', width: '200px' },
+  args: {
+    id: 'name',
+    width: '200px',
+    defaultValue: 6,
+    minValue: 0,
+    maxValue: 20,
+    step: 2,
+  },
   parameters: { controls: { exclude: baseProps } },
 } as Meta<CubeSliderProps>;
 
 const Template: Story<CubeSliderProps> = (args) => <Slider {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {};
 
-Default.args = {
-  defaultValue: 6,
-  minValue: 0,
-  maxValue: 20,
-  step: 2,
+export const Disabled = Template.bind({});
+Disabled.args = {
+  isDisabled: true,
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
   label: 'Slider',
 };
 
 export const Vertical = Template.bind({});
 Vertical.args = {
-  defaultValue: 4,
-  minValue: 0,
-  maxValue: 20,
-  step: 2,
   orientation: 'vertical',
-  label: 'Slider',
-  // labelPosition: 'side',
 };
