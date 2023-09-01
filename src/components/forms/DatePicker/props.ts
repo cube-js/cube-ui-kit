95d89eeb6e4d338a45bb4af2e27db99dfd44a973
Andrey Yamanov <tenphi@gmail.com>
2023-09-01 17:17:53 +0200

    fix(DateInput): force leading zeros

--- src/components/forms/DatePicker/props.ts
+++ src/components/forms/DatePicker/props.ts
@@ -1,19 +1,15 @@
 import { DatePickerProps } from '@react-types/datepicker';
 import { DateValue } from '@react-aria/calendar';
 
-export const DEFAULT_DATE_PROPS: Pick<
-  DatePickerProps<DateValue>,
-  'hideTimeZone' | 'hourCycle' | 'granularity'
-> = {
+export const DEFAULT_DATE_PROPS: Partial<DatePickerProps<DateValue>> = {
   granularity: 'day',
   hideTimeZone: true,
   hourCycle: 24,
+  shouldForceLeadingZeros: true,
 };
 
-export const DEFAULT_TIME_PROPS: Pick<
-  DatePickerProps<DateValue>,
-  'hideTimeZone' | 'hourCycle'
-> = {
+export const DEFAULT_TIME_PROPS: Partial<DatePickerProps<DateValue>> = {
   hideTimeZone: true,
   hourCycle: 24,
+  shouldForceLeadingZeros: true,
 };
