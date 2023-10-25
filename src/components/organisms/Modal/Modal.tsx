e06430c591fbb0ce93d946ef83a01542d1c4c635
Andrey Yamanov <tenphi@gmail.com>
2023-10-25 17:52:59 +0200

    chore: add react 18 support * 3

--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useCallback, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import styled from 'styled-components';
 import { CloseOutlined } from '@ant-design/icons';
 import { CSSTransition } from 'react-transition-group';
@@ -336,7 +336,13 @@ const modal: ModalService = {
       items = this.items;
     }
 
-    ReactDOM.render(
+    if (!this.root) {
+      return;
+    }
+
+    const root = createRoot(this.root);
+
+    root.render(
       items.map((item) => {
         const { id, onOk, onCancel, onClose, content, ...options } = item;
 
@@ -376,7 +382,6 @@ const modal: ModalService = {
           </Modal>
         );
       }),
-      this.root,
     );
   },
   open(item) {
