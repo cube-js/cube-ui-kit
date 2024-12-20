5da85027541a13973d867de0cd61b1496b638678
Andrey Yamanov <tenphi@gmail.com>
2024-12-20 14:42:08 +0100

    fix(Icon): rename from IconContainer * 3

--- src/icons/Icons.stories.tsx
+++ src/icons/Icons.stories.tsx
@@ -41,8 +41,7 @@ const Template: StoryFn<CubeIconProps> = (name) => {
       <Title>32px</Title>
       <Grid columns="repeat(auto-fit, 200px)" flow="row" gap="16px">
         {Object.keys(Icons).map((iconName) => {
-          if (iconName === 'IconContainer' || iconName === 'wrapIcon')
-            return null;
+          if (iconName === 'Icon' || iconName === 'wrapIcon') return null;
 
           const Icon = Icons[iconName];
 
