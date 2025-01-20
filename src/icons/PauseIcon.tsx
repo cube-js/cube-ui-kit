3685616ef456e3a019a57259fc22a0cd121d8e91
Andrey Yamanov <tenphi@gmail.com>
2025-01-20 15:03:01 +0100

    feat: new icons

---
+++ src/icons/PauseIcon.tsx
@@ -0,0 +1,19 @@
+import { wrapIcon } from './wrap-icon';
+
+export const PauseIcon = wrapIcon(
+  'PauseIcon',
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="none"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M8.493 8.42a.6.6 0 0 1 0-.84l3.48-3.536a.6.6 0 0 1 1.027.42v7.071a.6.6 0 0 1-1.028.421zm-5.024 0a.6.6 0 0 1 0-.84l3.48-3.536a.6.6 0 0 1 1.028.42v7.071a.6.6 0 0 1-1.028.421z"
+      clipRule="evenodd"
+    />
+  </svg>,
+);
