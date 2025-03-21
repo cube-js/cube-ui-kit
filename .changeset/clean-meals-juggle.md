14734139430af708d8b07776318b9baab23e9870
Andrey Yamanov <tenphi@gmail.com>
2025-03-21 12:21:14 +0100

    feat: add PlayCircleIcon and ReportIcon

---
+++ .changeset/clean-meals-juggle.md
@@ -0,0 +1,5 @@
+---
+'@cube-dev/ui-kit': patch
+---
+
+Add PlayCircle and Report icons.
---
+++ src/icons/ReportIcon.tsx
@@ -0,0 +1,20 @@
+import { wrapIcon } from './wrap-icon';
+
+export const ReportIcon = wrapIcon(
+  'ReportIcon',
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+    <g clipPath="url(#a)">
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M9 1H4v2h5zM3 1v2H1V1zM1 9V4h2v5zm0 1v5h2v-5zm3 0v5h5v-5zm6 0v5h5v-5zm5-1V4h-5v5zM9 9H4V4h5zm6-8v2h-5V1zM1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1z"
+        clipRule="evenodd"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h16v16H0z" />
+      </clipPath>
+    </defs>
+  </svg>,
+);
