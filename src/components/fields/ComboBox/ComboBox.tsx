218ca67a724c6f5d1c6544a653fd9980a6a5c8f4
Andrey Yamanov <tenphi@gmail.com>
2025-09-02 13:45:51 +0200

    fix(ComboBox): trigger button styles

--- src/components/fields/ComboBox/ComboBox.tsx
+++ src/components/fields/ComboBox/ComboBox.tsx
@@ -71,18 +71,14 @@ const TriggerElement = tasty({
     placeContent: 'center',
     placeSelf: 'stretch',
     radius: '(1r - 1bw) right',
-    width: {
-      '': '4x',
-      '[data-size="small"]': '3x',
-      '[data-size="medium"]': '4x',
-    },
+    width: '3x',
     color: {
       '': '#dark-02',
       hovered: '#dark-02',
       pressed: '#purple',
       '[disabled]': '#dark.30',
     },
-    border: 0,
+    border: 'left',
     reset: 'button',
     margin: 0,
     fill: {
