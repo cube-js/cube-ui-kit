216af806ae7dc5c29e73ed8a3803a21ad819ece2
Andrey Yamanov <tenphi@gmail.com>
2025-10-07 12:32:31 +0200

    feat: add ChartHeatmapIcon

---
+++ src/icons/ChartHeatmapIcon.tsx
@@ -0,0 +1,32 @@
+import { wrapIcon } from './wrap-icon';
+
+export const ChartHeatmapIcon = wrapIcon(
+  'ChartHeatmapIcon',
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path fill="currentColor" d="M2 8h7v4H2z" opacity=".3" />
+    <path fill="currentColor" d="M2 12h7v4H2z" opacity=".6" />
+    <path fill="currentColor" d="M9 3h6v5H9z" opacity=".1" />
+    <path fill="currentColor" d="M9 12h6v4H9z" opacity=".8" />
+    <path fill="currentColor" d="M9 8h6v4H9z" opacity=".4" />
+    <path fill="currentColor" d="M9 16h6v5H9z" opacity=".1" />
+    <path fill="currentColor" d="M15 12h7v4h-7z" opacity=".4" />
+    <path fill="currentColor" d="M15 8h7v4h-7z" opacity=".1" />
+    <rect
+      width="20"
+      height="18"
+      x="2"
+      y="3"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      rx="1"
+    />
+  </svg>,
+);
