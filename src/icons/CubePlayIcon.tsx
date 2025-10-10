db550387b3474f7c86eab5a6a4a6a2436e36790d
Andrey Yamanov <tenphi@gmail.com>
2025-10-10 16:32:20 +0200

    feat: add icons

---
+++ src/icons/CubePlayIcon.tsx
@@ -0,0 +1,20 @@
+import { wrapIcon } from './wrap-icon';
+
+export const CubePlayIcon = wrapIcon(
+  'CubePlayIcon',
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M21 13.5V7.991a1.98 1.98 0 0 0-1-1.717l-7-4.008a2.02 2.02 0 0 0-2 0L4 6.273c-.619.355-1 1.01-1 1.718v8.018c0 .709.381 1.363 1 1.717l7 4.008c.305.174.65.265 1 .265m0 0c.35 0 .695-.091 1-.265m-1 .265V12m0 0 8.73-5.04M12 12 3.27 6.96M17 16v6l5-3z"
+    />
+  </svg>,
+);
