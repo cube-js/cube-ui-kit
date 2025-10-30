778deef4e746fb92487d7f7cece3068b8acd430c
Andrey Yamanov <tenphi@gmail.com>
2025-10-30 14:56:50 +0100

    feat(ListBox): type prop * 2

--- src/components/fields/FilterListBox/FilterListBox.tsx
+++ src/components/fields/FilterListBox/FilterListBox.tsx
@@ -103,7 +103,7 @@ const StyledHeaderWithoutBorder = tasty(StyledHeader, {
 });
 
 export interface CubeFilterListBoxProps<T>
-  extends Omit<CubeListBoxProps<T>, 'filter'>,
+  extends Omit<CubeListBoxProps<T>, 'filter' | 'type'>,
     FieldBaseProps {
   /** Placeholder text for the search input */
   searchPlaceholder?: string;
