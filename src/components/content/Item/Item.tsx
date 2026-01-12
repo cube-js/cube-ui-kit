d69b3a90dde4e5a442ec828a6b48129745986d58
Andrey Yamanov <tenphi@gmail.com>
2026-01-12 17:57:35 +0100

    fix(Item): improve actions container collapse behavior on hover * 2

--- src/components/content/Item/Item.tsx
+++ src/components/content/Item/Item.tsx
@@ -480,7 +480,8 @@ const ItemElement = tasty({
       boxSizing: 'border-box',
       height: 'min ($size - 2bw)',
       width: {
-        '': 'max ($actions-width, 0px)',
+        '': 'max 0',
+        'has-actions-content': 'max ($actions-width, 0px)',
         'has-actions-content & show-actions-on-hover': 'max 0px',
         'has-actions-content & show-actions-on-hover & (:hover | :focus | :focus-within)':
           'max calc-size(max-content, size)',
