dc4e5ca45e6949c5285c1887bf6c871f1ad9ff20
Andrey Yamanov <tenphi@gmail.com>
2026-01-13 09:45:16 +0100

    feat: add SemanticQueryIcon

---
+++ src/icons/SemanticQueryIcon.tsx
@@ -0,0 +1,27 @@
+import { wrapIcon } from './wrap-icon';
+
+export const SemanticQueryIcon = wrapIcon(
+  'SemanticQueryIcon',
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <g clipPath="url(#a)">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M12 19v-5m0 5a2 2 0 0 0-2 2m2-2a2 2 0 0 1 2 2m-2-7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m2 7a2 2 0 1 1-4 0m4 0 5-2.5M14 11l5-2.5M14 3a2 2 0 1 1-4 0m4 0a2 2 0 1 0-4 0m4 0 5 2.5M10 11 5.057 8.529M10 21l-4.867-2.434M10 3 4.92 5.54M3 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 0v6m18-6a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 0v6M3 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4m18 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>,
+);
