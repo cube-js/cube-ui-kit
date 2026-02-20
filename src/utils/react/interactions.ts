21f9f1b1c1b323eaa59f3a6746e091917e6b8ece
Andrey Yamanov <tenphi@gmail.com>
2026-02-20 17:09:30 +0100

    fix(interactions): optimize react render

--- src/utils/react/interactions.ts
+++ src/utils/react/interactions.ts
@@ -1,24 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useFocus as reactAriaUseFocus, useFocusVisible } from 'react-aria';
 
 export function useFocus(
   { isDisabled }: { isDisabled?: boolean },
   onlyVisible = false,
 ) {
-  useEffect(() => {
-    setIsFocused(false);
-  }, [isDisabled]);
-
   let [isFocused, setIsFocused] = useState(false);
   let { isFocusVisible } = useFocusVisible({});
   let { focusProps } = reactAriaUseFocus({
     isDisabled,
-    // @ts-ignore
     onFocusChange: setIsFocused,
   });
 
   return {
     focusProps,
-    isFocused: isFocused && (onlyVisible ? isFocusVisible : true),
+    isFocused:
+      !isDisabled && isFocused && (onlyVisible ? isFocusVisible : true),
   };
 }
