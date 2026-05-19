a20cab22898be274180856894b6b597430973679
Andrey Yamanov <tenphi@gmail.com>
2026-05-19 11:41:10 +0200

    fix(TextInputBase): fill color

--- src/components/fields/TextInput/TextInputBase.tsx
+++ src/components/fields/TextInput/TextInputBase.tsx
@@ -63,7 +63,7 @@ export const INPUT_WRAPPER_STYLES: Styles = {
   gridRows: '1sf',
   placeItems: 'stretch',
   fill: {
-    '': '#surface',
+    '': '#surface-2',
     disabled: '#disabled-surface',
   },
   border: {
